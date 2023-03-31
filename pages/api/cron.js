1d34c7d999af9877125b38f6678dc4c9747b8388
Ash <ashman@basiq.io>
2023-03-31 12:43:24 +1100

    DX-304 Add-cron-job-test-cases-to-delete-users-for-past-1-month

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,3 +1,37 @@
-export default function handler(req, res) {
-    res.status(200).end('Hello Cron!');
-  }
+var token = process.env.SERVER_ACCESS;
+DestroyBulkUsersandtheirLifes(token);
+   function DestroyBulkUsersandtheirLifes(token) {
+     fetch("https://au-api.basiq.io/users", {
+         headers: { 
+             'Authorization': `Bearer ${token}`, 
+             'Accept': 'application/json'
+           }
+     })
+     .then(res => res.json())
+     .then((res) => {
+         console.log(res)
+         res.data.forEach((user) => {
+            // Fetch User Created date 1 week ago ... 
+            var strDate = user.createdTime;
+            var isoAPIdate = new Date(strDate).toISOString();
+            // Gets Current Date... 
+            const current = new Date();
+            // Gets One Week Old Date...
+                // current.setDate(current.getDate() - 7);
+            // Gets One Month Old Date...
+            current.setMonth(current.getMonth() - 1);
+            // Checks How many users are before 1 Month as Per Jim's comment here: https://basiq-io.slack.com/archives/CR7FCLUQN/p1675390597769379?thread_ts=1675327624.593949&cid=CR7FCLUQN
+            if ( isoAPIdate < current.toISOString()) {
+                //console.log(user.links.self);
+                fetch(`${user.links.self}`, {
+                 method: 'DELETE',
+                 headers: { 
+                     'Authorization': `Bearer ${token}`, 
+                     'Accept': 'application/json'
+                   }
+                 })
+            } 
+         })
+     })
+     .then(console.log("done"));
+ }
